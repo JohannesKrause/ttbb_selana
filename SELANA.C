04d260430672a6c392cae1aff1683fa0f9526298
Johannes Krause <johannes.krause1@mailbox.tu-dresden.de>
2017-07-20 16:06:51 +0200

    added different modes

--- SELANA.C
+++ SELANA.C
@@ -16,7 +16,7 @@ namespace SELAN{
   protected:
 
   private:
-    int m_check_dec;
+    size_t m_check_dec, m_modus;
     std::string m_inpath;
     std::string m_infile;
     std::string m_outpath;
@@ -52,6 +52,7 @@ namespace SELAN{
       reader.SetInputFile(infile+"|BEGIN_SELANA|END_SELANA");
       reader.SetComment("#");
       m_check_dec = reader.GetValue<int>("CHECK_DEC", 1);
+      m_modus = reader.GetValue<int>("SELMODUS", 1);
       msg_Debugging()<<METHOD<<"(): { mode \n" <<
                        "skip b's from hard decay handler:  " << m_check_dec <<
                        ") \n }" << std::endl;
@@ -65,19 +66,57 @@ namespace SELAN{
       // veto b-emissions from ME
       ATOOLS::Blob *sp(bl->FindFirst(ATOOLS::btp::Signal_Process));
       ATOOLS::Particle_Vector outvec(sp->GetOutParticles());
+      size_t numb_me(0);
       for (size_t i=0; i<outvec.size(); i++){
           ATOOLS::Particle * particle(outvec.at(i));
-          if (abs(particle->Flav().Kfcode())==5) return false;
-        }
+          if (abs(particle->Flav().Kfcode())==5) {
+              numb_me++;
+            }
 
+        }
       // veto b-emissions from the Shower which do not originate from the Hard Decay Handler if required
       ATOOLS::Blob *sh(bl->FindFirst(ATOOLS::btp::Shower));
       outvec=sh->GetOutParticles();
+      size_t numb_ps(0);
       for (size_t i(0); i<outvec.size();i++){
           ATOOLS::Particle * particle(outvec.at(i));
-          if ( (abs(particle->Flav().Kfcode())==5) && !m_check_dec ) return false;
-          if ( (abs(particle->Flav().Kfcode())==5) && !particle->Dec() ) return false;
+          //  if ( (abs(particle->Flav().Kfcode())==5) && !m_check_dec ) return false;
+          if ( (abs(particle->Flav().Kfcode())==5) && !particle->Dec() ){
+              numb_ps++;
+            }
         }
+      /*
+      if (numb_me>=1){
+          size_t numb_tot(0);
+          for (ATOOLS::Blob_List::iterator blit=bl->begin();
+               blit!=bl->end();++blit) {
+              ATOOLS::Blob* blob=*blit;
+              for (int i=0;i<blob->NOutP();i++) {
+                  ATOOLS::Particle *particle=blob->OutParticle(i);
+                  if (particle->DecayBlob()!=NULL) continue;
+                  if (abs(particle->Flav().Kfcode())==5) numb_tot++;
+                }
+            }
+
+          msg_Info() << "numb from ME=" << numb_me <<  " number of b from ps: " << numb_ps <<  ", total b:" << numb_tot << "       " << std::endl;
+        }
+       */
+
+      /*
+         modi:  1: default, veto all b emissions eiter from ME or PS
+                2: veto only ME-em with > 2 b's in ME
+                3: veto all b ME-em
+                4: veto Shower and ME, but not the "1 b from ME" case
+
+      */
+
+      if(m_modus==1 && ((numb_ps>0) || (numb_me>0))) return false;
+      if(m_modus==2 && numb_me>1) return false;
+      if(m_modus==3 && numb_me>0) return false;
+      if(m_modus==4){
+          if(numb_me==1) return true;
+          if (numb_me>0 || numb_ps>0) return false;
+      }
 
       return true;
     }
